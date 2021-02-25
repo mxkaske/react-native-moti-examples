dbea9739401f6cb57e60cfdcc69114d939432915
Maximilian Kaske <maximilian@kaske.org>
2021-02-25 23:35:08 +0100

    :construction: add basics

--- App.tsx
+++ App.tsx
@@ -1,47 +1,102 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function AnimatedStyleUpdateExample() {
-  const randomWidth = useSharedValue(10);
+  const [value, setValue] = useState(new Date());
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
+    return () => {
+      clearInterval(interval);
     };
-  });
+  }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }} />
+      <View style={s.container}>
+        <Numbers value={Math.floor(value.getHours() / 8)} maxValue={3} />
+        <Numbers value={value.getHours() % 10} maxValue={10} />
+        <Numbers value={Math.floor(value.getMinutes() / 10)} maxValue={6} />
+        <Numbers value={value.getMinutes() % 10} maxValue={10} />
+        <Numbers value={Math.floor(value.getSeconds() / 10)} maxValue={6} />
+        <Numbers value={value.getSeconds() % 10} maxValue={10} />
+      </View>
     </View>
   );
 }
+
+interface NumbersProps {
+  value: number;
+  maxValue: number;
+}
+
+const Numbers = ({ value, maxValue }: NumbersProps) => {
+  return (
+    <View style={[s.innerContainer, s.shadow]}>
+      <View style={[s.circle, s.shadow]} />
+      {Array(maxValue)
+        .fill(0)
+        .map((_, i) => {
+          const active = i === value;
+          return (
+            <View style={s.numberContainer}>
+              <Text key={i} style={[s.number, active && s.active]}>
+                {i}
+              </Text>
+            </View>
+          );
+        })}
+    </View>
+  );
+};
+
+const SPACING = 8;
+const DIAMETER = SPACING * 6;
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  innerContainer: {
+    backgroundColor: "#4B5563",
+    borderRadius: DIAMETER / 2,
+    marginHorizontal: SPACING,
+  },
+  numberContainer: {
+    width: DIAMETER,
+    height: DIAMETER,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  number: {
+    color: "black",
+  },
+  active: {
+    color: "white",
+  },
+  circle: {
+    position: "absolute",
+    alignSelf: "center",
+    backgroundColor: "black",
+    opacity: 0.4,
+    width: DIAMETER,
+    height: DIAMETER,
+    borderRadius: DIAMETER / 2,
+    transform: [{ scale: 1.2 }],
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+  },
+});
